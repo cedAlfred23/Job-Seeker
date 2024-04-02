dac29b438c3c28cb9c5aee30f9a87a24260866ee
cedAlfred23 <al.cedricgbaguidi@gmail.com>
2024-04-02 04:33:05 +0000

    allowed_hosts

--- xyz/settings.py
+++ xyz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yfsuishsfsfpjo2342'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'job-seeker-71ze.onrender.com']
 
 
 # Application definition
