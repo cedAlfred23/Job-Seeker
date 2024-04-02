543b99c79809a671a32db18766b3d34183964a0a
cedAlfred23 <al.cedricgbaguidi@gmail.com>
2024-04-02 05:02:10 +0000

    create superuser

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
