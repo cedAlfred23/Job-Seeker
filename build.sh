3661d737b416e4363f2e65e4602fd19d3d4c85ed
cedAlfred23 <al.cedricgbaguidi@gmail.com>
2024-04-02 05:20:34 +0000

    new create user

--- build.sh
+++ build.sh
@@ -10,5 +10,5 @@ python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]];
 then
-  python world_champ_2022/manage.py createsuperuser --no-input
+  python xyzCompany/manage.py createsuperuser --no-input
 fi
